c0d9e62368c7835d0d1cb5f9e6e85c950bfdc0ea
arturocuencac <arturocuencac@gmail.com>
2021-03-27 12:29:03 -0600

    Se adicionan JS para la navegación y el detalle de votos

---
+++ assets/js/diputaciones.js
@@ -0,0 +1,44 @@
+
+function combo_distrito() {
+  var x = document.getElementById("cbdistrito").value;
+  
+  if (x==1) {
+   window.location.href='./diputaciones_distrito.html';  
+  }    
+}
+
+ function combo_seccion() {
+  var x = document.getElementById("cbseccion").value;
+  
+  if (x==1) {
+   window.location.href='./diputaciones_distrito_seccion.html';  
+  } 
+}
+
+ function combo_casilla() {
+  var x = document.getElementById("cbcasilla").value;
+  
+  if (x==1) {
+   window.location.href='./diputaciones_distrito_casilla.html';  
+  } 
+}
+
+function combo_distrito_ppc() {
+  var x = document.getElementById("cbdistrito").value;
+  
+  if (x==1) {
+   window.location.href='./diputaciones_distrito_ppc.html';  
+  }    
+}
+
+function selecciona(valor) {
+  var  b1 = document.getElementById(valor);  
+
+  if (b1.classList.item(7) == null) {
+    b1.classList.add("seleccionado");
+    b1.innerHTML = "<p>Quitar Selección</p>";
+  } else {
+    b1.classList.remove("seleccionado");  
+    b1.innerHTML = "<p>Seleccionar</p>";
+  }
+}
